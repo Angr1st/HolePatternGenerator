7f7e20022194911c6cd1f82227c98ae2e465c551
Torben Clasen <t.clasen@outlook.com>
2022-11-06 23:19:52 +0100

    rename and refactor

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,7 @@ struct Config {
     hole_diameter:f64,
     plate_diameter:f64,
     distance_from_edge:f64,
+    padding_distance_from_edge:f64,
     target_file_name:String
 }
 
@@ -34,7 +35,7 @@ struct HolePosition {
 impl HolePosition {
     fn new(x:f64, z:f64) -> HolePosition {
         if x == 0.0 && z == 0.0 {
-            return HolePosition::createCenter()
+            return HolePosition::create_center()
         }
         else if x == 0.0 || z == 0.0 {
             return HolePosition { x, z, hole_type: HoleType::Axes }
@@ -44,7 +45,7 @@ impl HolePosition {
         }
     }
 
-    fn createCenter() -> HolePosition {
+    fn create_center() -> HolePosition {
         HolePosition { x: 0.0, z: 0.0, hole_type: HoleType::Center }
     }
 
@@ -95,6 +96,23 @@ fn read_config_from_file<P: AsRef<Path>>(path: P) -> Result<Config, Box<dyn Erro
     Ok(u)
 }
 
+fn insert_hole(i: i32, j: i32, hole_distance: f64, holes: &mut Vec<HolePosition>) {
+    let holeposition = HolePosition::new(i as f64 * hole_distance ,j as f64 * hole_distance);
+    let mirrored_opt = holeposition.mirror();
+    let rotated_opt = holeposition.rotate();
+    let mirrored_rotated_opt = holeposition.mirror().and_then(|m| m.rotate());
+    holes.push(holeposition);
+    if let Some(mirrored) = mirrored_opt {
+        holes.push(mirrored);
+    }
+    if let Some(rotated) = rotated_opt {
+        holes.push(rotated);
+    }
+    if let Some(mirrored_rotated) = mirrored_rotated_opt {
+        holes.push(mirrored_rotated);
+    }
+}
+
 fn main() -> Result<(),Box<dyn Error>> {
     let cli = Cli::parse();
 
@@ -105,7 +123,7 @@ fn main() -> Result<(),Box<dyn Error>> {
     //Calculate radius of plate 29
     let plate_radius = config.plate_diameter / 2.0;
     //and subtract the distance from the edege 28.15
-    let padded_plate_radius = plate_radius - config.distance_from_edge;
+    let padded_plate_radius = plate_radius - config.padding_distance_from_edge;
     //compute distance between 2 holes center points
     let hole_distance = config.hole_distance + config.hole_diameter;
     //compute the amount of holes 
@@ -114,19 +132,9 @@ fn main() -> Result<(),Box<dyn Error>> {
     let r_hole_amount = hole_amount.floor() as i32;
 
     for i in 0..=r_hole_amount {
-        let holeposition = HolePosition::new(i as f64 * hole_distance ,0.0);
-        let mirrored_opt = holeposition.mirror();
-        let rotated_opt = holeposition.rotate();
-        let mirrored_rotated_opt = holeposition.mirror().and_then(|m| m.rotate());
-        holes.push(holeposition);
-        if let Some(mirrored) = mirrored_opt {
-            holes.push(mirrored);
-        }
-        if let Some(rotated) = rotated_opt {
-            holes.push(rotated);
-        }
-        if let Some(mirrored_rotated) = mirrored_rotated_opt {
-            holes.push(mirrored_rotated);
+        //insert_hole(i,0, hole_distance, &mut holes);
+        for j in 0..=r_hole_amount {
+            insert_hole(i, j, hole_distance, &mut holes)
         }
     }
 
